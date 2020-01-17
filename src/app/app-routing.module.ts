213dda74fca29e4802fe6c134370cf6299dce8ca
Eissa Abdellah <eissa2020482@gmail.com>
2020-01-17 22:18:15 +0200

    add user (login , regigster)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { UsersComponent } from './users/users.component';
+import { LoginFormComponent } from './users/login-form/login-form.component';
+import { RegisterFormComponent } from './users/register-form/register-form.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'user',component:UsersComponent,
+  children:[{path:'login',component:LoginFormComponent},{path:'register',component:RegisterFormComponent}]}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,19 @@ import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { JewelryComponent } from './jewelry/jewelry.component';
 import { BagsComponent } from './bags/bags.component';
+import { UsersComponent } from './users/users.component';
+import { LoginFormComponent } from './users/login-form/login-form.component';
+import { RegisterFormComponent } from './users/register-form/register-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
     JewelryComponent,
-    BagsComponent
+    BagsComponent,
+    UsersComponent,
+    LoginFormComponent,
+    RegisterFormComponent
   ],
   imports: [
     BrowserModule,

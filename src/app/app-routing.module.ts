1302db5d02683de02ee49b5113252669bf6f7664
amira <amiraabdelhady885@gmail.com>
2020-01-19 15:02:19 +0200

    adding routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,16 @@ import { LoginFormComponent } from './users/login-form/login-form.component';
 import { RegisterFormComponent } from './users/register-form/register-form.component';
 import { JewelrydetailsComponent } from './jewelrydetails/jewelrydetails.component';
 import { BagsdetailsComponent } from './bagsdetails/bagsdetails.component';
+import { BagsComponent } from './bags/bags.component';
+import { HomeComponent } from './home/home/home.component';
+import { JewelryComponent } from './jewelry/jewelry.component';
+import { ClothingComponent } from './clothing/clothing.component';
+
 const routes: Routes = [
+  {path:'',component:HomeComponent}, 
+  {path:'bags',component:BagsComponent},
+  {path:'clothing',component:ClothingComponent}, 
+  {path:'jewelry',component:JewelryComponent},
   {path:'user',component:UsersComponent,
   children:[{path:'login',component:LoginFormComponent},{path:'register',component:RegisterFormComponent}]},
   {path:'jewelrydetail',component:JewelrydetailsComponent},

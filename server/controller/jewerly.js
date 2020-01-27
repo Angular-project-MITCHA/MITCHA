556d694ffedc5b3d5443fac0bcf273d2f140a9d9
Asmaa Marzouk <59687595+AsmaaMarzouk@users.noreply.github.com>
2020-01-27 15:57:03 +0200

    Update bags database and change some styles

--- server/controller/jewerly.js
+++ server/controller/jewerly.js
@@ -9,9 +9,28 @@ var parseUrlencoded = bodyParser.urlencoded({
 });
 
 route.get('/list', function (req, resp) {
- mongoose.model('jewerly').find(function (err, data) {
+  console.log(req.query)
+  const pageSize= +req.query.pagesize;
+  const currentPage= +req.query.page;
+  let fetchedBags;
+  const mong=mongoose.model('jewerly').find(function (err, data) {
      resp.json(data);
    })
+  //  const mong=mongoose.model('jewerly').find() 
+   if(pageSize && currentPage){
+    mong.skip(pageSize * (currentPage - 1))
+  .limit(pageSize)
+  }
+  mong.then(comingBags =>{
+    fetchedBags=comingBags;
+    return mongoose.model('jewerly').count()
+   //  resp.status(200).json(po)
+  }).then(count =>{
+   resp.status(200).json({
+     cbag:fetchedBags,
+     maxBags:count
+   })
+  })  
 })
 
 route.get('/jewelrydetails/:_id', function (req, resp) {

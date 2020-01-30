1a3c0b86503dc79b0e0492f35c6fe60de98d8a40
Eissa Abdellah <eissa2020482@gmail.com>
2020-01-30 09:07:06 +0200

    updatesss

--- server/controller/bags.js
+++ server/controller/bags.js
@@ -47,6 +47,26 @@ router.get('/bagdetail/:_id',function(req,resp){
  })
 
 })
+
+
+
+// search bag name
+
+router.get('/search/:name',function(req,resp){
+
+     
+  var name=req.params.name;
+ 
+ mongoose.model('bags').find({"name": {"$regex": name}},function(err,data){
+  if(data.length!=0)
+
+  resp.json(data);
+  else
+  resp.send("Not found");
+
+ })
+
+})
  
 
 module.exports = router;
